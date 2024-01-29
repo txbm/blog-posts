47670b6da6194ed16f7e2646bd4d9614de1fc323
Peter M. Elias <petermelias@gmail.com>
2024-01-29 12:53:47 -0600

    update code sample

--- rust-patterns-for-lifetime-management/example_4.rs
+++ rust-patterns-for-lifetime-management/example_4.rs
@@ -1,7 +1,15 @@
-fn main() {
-    let client = HttpClient::new();
-    
-    tokio::spawn(async move {
-        client.get(...)
-    })
+use reqwest::Client;
+
+const URL: &str = "https://google.com";
+
+#[tokio::main]
+async fn main() {
+    let client = Client::new();
+
+    tokio::spawn(async move { client.get(URL).send().await.unwrap() })
+        .await
+        .unwrap();
+
+    // `client` is no longer a valid reference at this point
+    // it was permanently moved into the `spawn`'d closure
 }
